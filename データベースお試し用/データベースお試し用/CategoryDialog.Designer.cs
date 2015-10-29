35b34ce0a965000318846714dcd7ce79d7270e92
Ryouta Ozaki <r.ozaki@quantum-g.jp>
2015-10-29 15:45:29 +0900

    分類管理ウインドウを追加

---
+++ データベースお試し用/データベースお試し用/CategoryDialog.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace データベースお試し用
+{
+    partial class CategoryDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.newBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 48);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(474, 228);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // newBtn
+            // 
+            this.newBtn.Location = new System.Drawing.Point(310, 12);
+            this.newBtn.Name = "newBtn";
+            this.newBtn.Size = new System.Drawing.Size(75, 23);
+            this.newBtn.TabIndex = 1;
+            this.newBtn.Text = "新規";
+            this.newBtn.UseVisualStyleBackColor = true;
+            this.newBtn.Click += new System.EventHandler(this.newBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(391, 12);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.deleteBtn.TabIndex = 2;
+            this.deleteBtn.Text = "削除";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // CategoryDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 288);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.newBtn);
+            this.Controls.Add(this.listView1);
+            this.Name = "CategoryDialog";
+            this.Text = "分類管理";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button newBtn;
+        private System.Windows.Forms.Button deleteBtn;
+    }
+}
---
+++ データベースお試し用/データベースお試し用/CategoryDialog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace データベースお試し用
+{
+    public partial class CategoryDialog : Form
+    {
+        private List<Category> categories;
+
+        public CategoryDialog(List<Category> categories)
+        {
+            this.categories = categories;
+
+            InitializeComponent();
+            foreach (var category in categories)
+            {
+                this.listView1.DataBindings.Add("Text", category, "Name");
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newBtn_Click(object sender, EventArgs e)
+        {
+            this.listView1.DataBindings.Add("Text", new Category(), "Name");
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
